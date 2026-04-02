04ee1f2834f767dbcef7c5732969c30976ac06c7
REG-BUU <parichat.la@go.buu.ac.th>
2026-04-02 13:59:21 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,19 +27,15 @@
 
 
     // Hero Header carousel
-    $(".header-carousel").owlCarousel({
-        animateOut: 'slideOutDown',
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-    });
+$('.header-carousel').owlCarousel({
+    autoplay: true,
+    smartSpeed: 800,
+    items: 1,
+    loop: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    dots: true
+});
 
 
     // International carousel
